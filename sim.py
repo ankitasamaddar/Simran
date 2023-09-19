ade5933d9b612759edf87b38fbcb8094ee727feb
ankitasamaddar <ankitasamaddar536@gmail.com>
2023-09-19 21:50:30 +0530

    SIMPLE DATA TYPES

---
+++ sim.py
@@ -0,0 +1,44 @@
+"""
+Description: [A brief description of the file's purpose]
+Author: [Your name]
+Date: [Creation or modification date]
+Usage: [How to use or run the file, if applicable]
+"""
+
+
+#SIMPLE DATA TYPES
+# Declare and initialize the variable with a name
+name = "Simran"  # Replace "John" with your first name
+
+# Print the value and datatype of the variable
+print(f"value: {name} type: {type(name).__name__}")
+# Declare and initialize the variable for Manitoba Driver's Licence status
+has_licence = True  # Change to False if you don't have a valid licence
+
+# Print the value and datatype of the variable
+print(f"value: {has_licence} type: {type(has_licence).__name__}")
+
+# Declare and initialize the variable for the current year
+current_year = 2023  
+
+# Print the value and datatype of the variable
+print(f"this year: {current_year} type: {type(current_year).__name__}")
+
+
+# Add 1 to the year to get the next year
+next_year = current_year + 1
+
+# Print the value and datatype of the next year
+print(f"next year: {next_year} type: {type(next_year).__name__}")
+
+#CALCULATIONS
+
+
+#LISTS
+
+#TUPLES
+
+#DICTIONARIES
+
+#SETS
+
